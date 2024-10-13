318de5e222cd38a57c2bc8d6b649fc752056c938
parik <parik@DESKTOP-TK4RI84>
2024-10-14 03:15:37 +0530

    initialize Client project and impliment Basic Architecture

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen();
 string filePath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).FullName, "Infrastructure", "Data", "contacts.json");
 // Register all the service
 builder.Services.AddScoped<IContactService, ContactService>(provider => new ContactService(filePath));
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // custom error handling middleware
@@ -23,6 +23,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors(x =>
+{
+    x.AllowAnyMethod()
+          .AllowAnyHeader()
+    .WithOrigins("http://localhost:4200", "http://localhost:4200");
+});
+
 app.MapControllers();
 
 app.Run();
